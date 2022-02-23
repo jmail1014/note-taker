b8611f51a0c43f0f0b76091d33edfa85001981f0
jclong <j.mail1014@gmail.com>
2022-02-23 15:54:51 -0500

    add routes to noteRoutes.js

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,2 +1,42 @@
-const router = require('express').Router();
-const { filterByQuery, findById, createNewNote }
+const { route } = require("express/lib/application");
+const req = require("express/lib/request");
+const { notes } = require("../../lib/notes");
+
+const router = require("express").Router();
+const { createNewNote, findById, deleteNote } = require("../../lib/notes");
+
+// Get all notes
+router.get("/notes", (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+// Get note by id
+router.get("/notes/:id", (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (getId) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+// Create new note
+router.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNotes(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+// Delete note
+router.delete("/notes/:id", (req, res) => {
+  deleteNote(notes, req.params.id);
+  res.json(notes);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes';)
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-const { notes } = require('./db/db.json');
+const { notes } = require("./db/db.json");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(3001, () => {
   console.log(`API server now on port 3001!`);
